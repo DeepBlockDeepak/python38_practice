c2de9182bf54359ef1aba09a09755b7d41adb184
DeepBlockDeepak <jordan.medina1729@gmail.com>
2021-01-07 00:18:44 -0700

    finished the factorialLog.py script. Serves as a useful template for storing error logs in the cwd of the script in separate .txt files

--- Chapter10/errorExample.py
+++ Chapter10/errorExample.py
@@ -1,10 +1,12 @@
-import traceback
+import traceback #needed when using "except:" on line 6
 
 try:
-    raise Exception("This is the error message.")
+    raise Exception("This is the error message. Hallelujah Satan")
 
-except:
+except Exception as err:    # alternatively, just use--->  except:
+                            #if using ---> except: ---> follow with ---> errorFile.write(traceback.format_exc())
     errorFile = open("errorinfo.txt", 'w')
-    errorFile.write(traceback.format_exc())
+    errorFile.write(str(err))   #must use str() on 'err' 
+    #errorFile.write(traceback.format_exc())
     errorFile.close()
     print("The traceback info was written to errorInfo.txt")
--- Chapter10/factorialLog.py
+++ Chapter10/factorialLog.py
@@ -1,27 +1,51 @@
-import logging
-#Include the following line to remove all log messages within the script
+import logging, os
+
+# level applied to disable() (DEBUG, INFO, WARNING, ERROR, CRITICAL) __    
+        # precludes all smaller ranked logging messages from appearing in the script
 #logging.disable(logging.CRITICAL)
 
-#following are the logging levels following logging.debug()
-"""logging.info("")
+"""
+#####################################################################################
+#   Logging Levels                                                                  #
+#       -- like a hierarchy for the 'level' parameter in logging.basicConfig()'     #
+#                                                                                   #
+#####################################################################################
+
+loggin.debug()    #smallest rank...Everything == debug and 'above' debug will be printed if .DEBUG is used in Config
+logging.info("") 
 logging.warning("")
 logging.error("")
 logging.critical("")
 """
+#Search the cwd for fresh filepath names 
+error_file_num = 1
+while True:        #os.path.abspath('.') is the directory which contains the current .py file
+    error_log_filepath = os.path.join(  #create the file path for the error log
+        os.path.abspath('.'),
+        ("factorialLogpy_errorfile_" + str(error_file_num) + ".txt")
+    ) 
 
-logging.basicConfig(level=logging.DEBUG, format= " %(asctime)s - %(levelname)s - %(message)s")
+    if not os.path.exists(error_log_filepath): #if the filepath is novel, then it's good to go
+        break
+    error_file_num += 1 #if the filepath isn't novel, iterate it's basename number
+
+#the parameter 'filename = error_log_filepath' saves the logging content to novel files 
+logging.basicConfig(filename = error_log_filepath, level = logging.DEBUG, format = "%(asctime)s - %(levelname)s - %(message)s")
 logging.debug("Start of program")
 
+############################################################################################################
+#BUG: Not a bug, but an idea... Repleace the factorial() function with something that is useful for tracking
 def factorial(n):
-    logging.debug("Start of Factorial function")
     total = 1
+    logging.debug("Start of Factorial function. Total = {}".format(total))
 
     for i in range(1, n + 1):
         total *= i
         logging.debug("The value of i is {}. Total is {}".format(i , total))
 
     logging.debug("End of factorial loop")
+    
     return total
 
-print(factorial(5))
-logging.debug("end of the entire script")
+factorial(5)
+logging.critical("End of the entire script")
