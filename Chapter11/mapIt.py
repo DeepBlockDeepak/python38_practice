445cadbbc714f499dc127c76fe3f3e4677ec418d
DeepBlockDeepak <jordan.medina1729@gmail.com>
2021-01-08 16:37:13 -0700

    didn't change anyting to mapIt.py

--- Chapter11/mapIt.py
+++ Chapter11/mapIt.py
@@ -4,14 +4,13 @@ if len(sys.argv) <= 1:
     address = pyperclip.paste()
     #address should be a string who's items are separated by spaces
     address = "+".join(address.split(" "))
-    url = "www.google.com/maps/place/" + address
+    print(address)
 
 
 else:
 
     address = "+".join(sys.argv[1:]) #for the Google Map URL
 
-    url = "www.google.com/maps/place/" + address
 
-
+url = "www.google.com/maps/search/" + address
 webbrowser.open(url)
