fc2485361ccba7d5255eecd95bbd934a28a3ee35
DeepBlockDeepak <jordan.medina1729@gmail.com>
2021-01-15 15:31:48 -0700

    Fully commented the source code, for clearer explanation of the logic

--- DailyCodingQ/string_delimeter_reverser_2.py
+++ DailyCodingQ/string_delimeter_reverser_2.py
@@ -2,37 +2,50 @@
 
 def string_reverser2(input_string):
 
-    d_list = []
-    w_list = []
+    d_list = []     #holds delim_string elements
+    w_list = []     #holds word_string elements
 
-    delim_string = ''
-    word_string = ''
+    delim_string = ''   #container for consecutive delimeters, encountered in the input_string
+    word_string = ''    #container for consecutively encountered non-delimeters, encountered in input_string
 
-    set_of_delimeters = ["/", ";", ":", "//", "\\", "-"]
+    set_of_delimeters = ["/", ";", ":", "//", "\\", "-"]    #list of possible delimeters
 
-    for char in input_string:
+    for char in input_string:   
+        """
+        loop over each char of the input string. 
+        Logic will be broken into consecutively encountered delimeters and consecutively encountered non-delimeters
+        """
 
-        if char in set_of_delimeters:
-            if delim_string == '':
-                w_list.append(word_string)
-                word_string = ''
-            delim_string = delim_string + char
+        if char in set_of_delimeters:   #delimeter encountered
+            if delim_string == '':      #when the current char is the first delimeter of the current pass
+                w_list.append(word_string)  #because the current char is the first delimeter of the new interval, 
+                word_string = ''            #that means we finished processing all the 'word' chars of the previous interval.
+                                            #Append the word_string to the word_list and reset word_string for the next batch. 
             
-        else:
-            if word_string == '':
+            delim_string = delim_string + char  #add the delimeter to the delim_string
+            
+        else:   #non-delimeter encountered
+            if word_string == '':       #when the current char is the first non-delimeter of the current pass
                 d_list.append(delim_string)
                 delim_string = ''
             word_string = word_string + char
-            
-    if delim_string != '':
+    
+    #Catch any leftover delimeters or words... This will occur for whomever comprises the last interval of the input_string
+    if delim_string != '':  #when a delimeter is the last interval
         d_list.append(delim_string)
     if word_string != '':
         w_list.append(word_string)
 
-    w_list = list(reversed(w_list))
+    w_list.reverse()    #necessary reversal of the words_list
 
-    final = ''
+    final = ''          #final string to contain the correctly ordered result of delimeters and words
 
+
+    """
+    Words_list and Delimter_list will often not be the same length. 
+    It is necessary to loop over the range of the longest length, ignoring when an out-of-bounds loop is 
+    encountered in the shorter list by appending a blank string element to the appropriate string
+    """
     for i in range(max(len(w_list), len(d_list))):
         
         word_string = w_list[i] if i < len(w_list) else ''
@@ -43,7 +56,7 @@ def string_reverser2(input_string):
 
 test1 = 'hello/world:here'  #here/world:hello
 test2 = 'hello/world:here/' #here/world:hello/
-test3 = 'hello//world:here' #here//world:hello
+test3 = ':hello//world:here;;' #:here//world:hello;;
 
 for test in [test1, test2, test3]:
     print("\t/ " + "-"*len(test) + " \\")
